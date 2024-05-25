671fe55f3e3410a7157fd2c73164e1318732c22d
Nekai <alessandrobelotti4570@gmail.com>
2024-05-25 13:37:31 +0200

    Delete obsolete Nekai.Interactivity.

--- Nekai.Interactivity/Commands/Attributes/CommandAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nekai.Interactivity;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
-public class CommandAttribute : BaseCommandDataAttribute
-{
-	public CommandAttribute(string command, params string[] aliases) : base(command, aliases)
-	{
-	}
-}
