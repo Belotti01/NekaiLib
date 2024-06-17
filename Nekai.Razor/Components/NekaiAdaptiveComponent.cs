fabcc7d4ffd41eb04bb5e4bc3808b8e4cfe23f65
Nekai <alessandrobelotti4570@gmail.com>
2024-06-17 15:56:43 +0200

    Add NekaiGrid, NekaiCard, NekaiGridItem.

---
+++ Nekai.Razor/Components/NekaiAdaptiveComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nekai.Razor
+{
+    public class NekaiAdaptiveComponent : NekaiComponentBase
+    {
+        /// <inheritdoc cref="FluentGridItem.xxl" />
+        public int? Xxl { get; set; }
+        /// <inheritdoc cref="FluentGridItem.xl" />
+		public int? Xl { get; set; }
+        /// <inheritdoc cref="FluentGridItem.lg" />
+		public int? Lg { get; set; }
+        /// <inheritdoc cref="FluentGridItem.md" />
+		public int? Md { get; set; }
+        /// <inheritdoc cref="FluentGridItem.sm" />
+		public int? Sm { get; set; }
+        /// <inheritdoc cref="FluentGridItem.xs" />
+		public int? Xs { get; set; }
+	}
+}
