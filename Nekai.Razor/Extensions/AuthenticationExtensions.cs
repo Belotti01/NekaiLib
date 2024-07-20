1d29886dd87f5b81ccce97f6676e246b2c6d74bd
Nekai <alessandrobelotti4570@gmail.com>
2024-07-20 17:49:22 +0200

    Add extension methods to retrieve the currently logged in user's ID.

---
+++ Nekai.Razor/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Nekai.Razor;
+
+public static class AuthenticationExtensions
+{
+	/// <summary>
+	/// Get the currently logged in user's ID as a <see langword="string"/>.
+	/// </summary>
+	/// <param name="auth"> The claims' source. </param>
+	/// <returns> The currently logged in user's ID, or <see langword="null"/> if the user is not authenticated. </returns>
+	public static async Task<string?> GetCurrentUserIdAsync(this AuthenticationStateProvider auth)
+	{
+		var authState = await auth.GetAuthenticationStateAsync();
+
+		var claimsIdentity = (ClaimsIdentity?)authState.User.Identity;
+		var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+		var userIdString = claim?.Value;
+
+		if(userIdString is null)
+			return default;
+
+		return userIdString;
+	}
+
+	/// <summary>
+	/// Get the currently logged in user's ID as a <typeparamref name="TKey"/>.
+	/// </summary>
+	/// <typeparam name="TKey"> The parsable type of the ID. </typeparam>
+	/// <param name="auth"> The claims' source. </param>
+	/// <param name="format"> The format used for parsing. </param>
+	/// <returns> The currently logged in user's ID, or <see langword="null"/> if the user is not authenticated. </returns>
+	public static async Task<TKey?> GetCurrentUserIdAsync<TKey>(this AuthenticationStateProvider auth, IFormatProvider? format = null)
+		where TKey : IParsable<TKey>
+	{
+		var userIdString = await GetCurrentUserIdAsync(auth);
+		if(userIdString is null)
+			return default;
+
+		if(!TKey.TryParse(userIdString, format, out TKey? userId))
+			return default;
+
+		return userId;
+	}
+}
