adae1014df8e4d9904c6fee4feffb2c54d0d1750
Nekai <alessandrobelotti4570@gmail.com>
2024-09-23 16:30:24 +0200

    String extension methods fixes.

--- Nekai.Common/Extensions/Strings/StringAnalisysExtensions.cs
+++ Nekai.Common/Extensions/Strings/StringAnalisysExtensions.cs
@@ -2,14 +2,6 @@
 
 public static class StringAnalisysExtensions
 {
-	/// <inheritdoc cref="OnlyContains(ReadOnlySpan{char}, char[], StringComparison)"/>
-	public static bool OnlyContains(this ReadOnlySpan<char> str, StringComparison comparison, params char[] characters)
-		=> str.OnlyContains(characters, comparison);
-
-	/// <inheritdoc cref="OnlyContains(ReadOnlySpan{char}, char[], StringComparison)"/>
-	public static bool OnlyContains(this ReadOnlySpan<char> str, params char[] characters)
-		=> str.OnlyContains(characters);
-
 	/// <summary>
 	/// Check whether the <see cref="ReadOnlySpan{T}"/> only contains the characters
 	/// in <paramref name="characters"/>.
@@ -28,47 +20,44 @@ public static class StringAnalisysExtensions
 
 	/// <inheritdoc cref="ContainsAll(ReadOnlySpan{char}, char[], StringComparison)"/>
 	public static bool ContainsAll(this ReadOnlySpan<char> str, params char[] characters)
-		=> str.ContainsAll(characters);
-
-	/// <inheritdoc cref="ContainsAll(ReadOnlySpan{char}, char[], StringComparison)"/>
-	public static bool ContainsAll(this ReadOnlySpan<char> str, StringComparison comparison, params char[] characters)
-		=> str.ContainsAll(characters, comparison);
+		=> str.ContainsAll(characters, StringComparison.Ordinal);
 
 	/// <summary>
 	/// Check whether a <see cref="ReadOnlySpan{T}"/> contains all the specified <paramref name="characters"/>.
 	/// </summary>
 	/// <param name="str">The span to check.</param>
 	/// <param name="characters">The characters to find in <paramref name="str"/>.</param>
-	/// <param name="comparison">The comparer to use.</param>
+	/// <param name="comparisonType">The comparer to use.</param>
 	/// <returns><see langword="true"/> if the <paramref name="str"/> contains all the specified <paramref name="characters"/>;
 	/// <see langword="false"/> otherwise.</returns>
-	public static bool ContainsAll(this ReadOnlySpan<char> str, char[] characters, StringComparison comparison = StringComparison.Ordinal)
+	public static bool ContainsAll(this ReadOnlySpan<char> str, char[] characters, StringComparison comparisonType = StringComparison.Ordinal)
 	{
-		str.All(c => characters.Any(x => x.Equals(c, comparison)));
+		str.All(c => characters.Any(x => x.Equals(c, comparisonType)));
 		for(int i = str.Length - 1; i >= 0; i--)
 		{
-			if(!str.Contains(characters[i], comparison))
+			if(!str.Contains(characters[i], comparisonType))
 				return false;
 		}
 		return true;
 	}
 
 	/// <summary>
-	/// Check whether a <see cref="ReadOnlySpan{T}"/> contains the specified <paramref name="character"/>.
+	/// Check whether a <see cref="ReadOnlySpan{T}"/> contains the specified <paramref name="value"/>.
 	/// </summary>
 	/// <param name="str">The span to check.</param>
-	/// <param name="character">The character to find in <paramref name="str"/>.</param>
-	/// <param name="comparison">The comparer to use.</param>
+	/// <param name="value">The character to find in <paramref name="str"/>.</param>
+	/// <param name="comparisonType">The comparer to use.</param>
 	/// <returns><see langword="true"/> if the <paramref name="str"/> contains at least one instance of the specified
-	/// <paramref name="character"/>; <see langword="false"/> otherwise.</returns>
-	public static bool Contains(this ReadOnlySpan<char> str, char character, StringComparison comparison = StringComparison.Ordinal)
-		=> str.IndexOf(character, comparison) >= 0;
+	/// <paramref name="value"/>; <see langword="false"/> otherwise.</returns>
+	public static bool Contains(this ReadOnlySpan<char> str, char value, StringComparison comparisonType = StringComparison.Ordinal)
+		=> str.IndexOf(value, comparisonType) >= 0;
 
-	public static int IndexOf(this ReadOnlySpan<char> str, char character, StringComparison comparison = StringComparison.Ordinal)
+	/// <inheritdoc cref="string.IndexOf(char, StringComparison)"/>
+	public static int IndexOf(this ReadOnlySpan<char> str, char value, StringComparison comparisonType = StringComparison.Ordinal)
 	{
 		for(int i = str.Length - 1; i >= 0; --i)
 		{
-			if(str[i].Equals(character, comparison))
+			if(str[i].Equals(value, comparisonType))
 				return i;
 		}
 		return -1;
