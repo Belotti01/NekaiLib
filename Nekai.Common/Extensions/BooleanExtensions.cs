dc09884d63522f9c209f81c2b3845e7401d75368
Nekai <alessandrobelotti4570@gmail.com>
2025-06-27 11:01:20 +0200

    Remove debug test for missing boolean.ToString implementations.

--- Nekai.Common/Extensions/BooleanExtensions.cs
+++ Nekai.Common/Extensions/BooleanExtensions.cs
@@ -4,31 +4,6 @@ namespace Nekai.Common;
 
 public static class BooleanExtensions
 {
-#if DEBUG
-	static BooleanExtensions()
-	{
-		_CheckBoolStringTypeForMissingImplementations();
-	}
-
-	[Conditional("DEBUG")]
-	private static void _CheckBoolStringTypeForMissingImplementations()
-	{
-		BoolStringType[] types = Enum.GetValues<BoolStringType>();
-		string s;
-
-		foreach(var t in types)
-        {
-			try
-			{
-				s = true.ToString(t);
-				s = false.ToString(t);
-			}catch(NotImplementedException)
-			{
-				Debug.Fail($"Missing implementation of {nameof(BoolStringType)} \"{t}\" in extension method \"{nameof(BooleanExtensions)}.{nameof(ToString)}\".");
-			}
-        }
-    }
-#endif
 
 	public static string ToString(this bool value, BoolStringType type)
 		=> type switch
